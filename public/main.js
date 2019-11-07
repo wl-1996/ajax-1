051cf9ee48f7bb0cd76c67b6c93dbbb55b3a299f
WK <741341649@qq.com>
2019-11-07 10:34:18 +0800

    修改部分东西

--- public/main.js
+++ public/main.js
@@ -52,32 +52,43 @@ getXML.onclick = () => {
   const request = new XMLHttpRequest();
   request.open("GET", "/4.xml");
   request.onreadystatechange = () => {
-    console.log(request.readyState);
+    // console.log(request.readyState);
     if (request.readyState === 4) {
       if (request.status >= 200 && request.status < 300) {
         const dom = request.responseXML;
+        // 找到标签名为warning的第一个元素，获取它的文本内容
         const text = dom.getElementsByTagName("warning")[0].textContent;
-        console.log(text.trim());
+        console.log(text.trim()); //.trim()方法删除字符串两端的空格
       }
     }
   };
   request.send();
 };
+
 getJSON.onclick = () => {
   const request = new XMLHttpRequest();
   request.open("GET", "/5.json");
+  // 下面的onreadystatechange函数用到了“回调”，当readyState码变为4时浏览器会回头调用这个函数，得到服务器响应内容：request.response
   request.onreadystatechange = () => {
-    console.log(request.readyState);
+    // console.log(request.readyState);
     if (request.readyState === 4 && request.status === 200) {
-      console.log(typeof request.response);
-      console.log(request.response);
+      // console.log(typeof request.response);
+      // console.log(request.response);
+      // JSON.parse()方法把会把一个JSON字符串转换成JS的数值或是物件：
       const bool = JSON.parse(request.response);
-      console.log(typeof bool);
-      console.log(bool);
+      // console.log(typeof bool);
+      // console.log(bool);
     }
   };
   request.send();
+  // 发送请求后立即打印出响应内容是空的，因为异步，此时收不到响应：
+  console.log(request.response);
+  // 2s后会收到响应，此时才能打印出响应内容；也可以把这行代码放到onreadystatechange函数里面：
+  setTimeout(() => {
+    console.log(request.response);
+  }, 2000);
 };
+
 let n = 1;
 getPage.onclick = () => {
   const request = new XMLHttpRequest();
